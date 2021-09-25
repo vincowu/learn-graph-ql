37a23dbe756c8164453c55afe499f53a08ecb2b7
Vinco Wu <vwu13@outlook.com>
2021-09-24 22:14:09 -0400

    basic front end structure after creating react app

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './partials/main.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Reading List</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BookList extends Component {
+    render() {
+        return (
+            <div className="bookList">
+
+            </div>
+        )
+    }
+}
+
+export default BookList
