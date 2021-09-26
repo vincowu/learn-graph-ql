b1704661969380727eb37c7a7ad336af13ad5616
Vinco Wu <vwu13@outlook.com>
2021-09-26 19:09:17 -0400

    created booklist component, still working on AddBooks component

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 // allows express to understand graphQL
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const app = express();
+
+// allow cross origin request
+app.use(cors())
 
 // connect to database
 mongoose.connect("mongodb+srv://dbUser:rootroot@cluster0.orpkm.mongodb.net/graphql?retryWrites=true&w=majority");
